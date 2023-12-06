93e712f4da072a507038b4f941a45ea83ac9f456
Yang Cao <yangcao@umich.edu>
2023-12-06 20:05:24 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 from flask import url_for
+
+# 页面填充数据
+name = "Yang Cao"
+movies = [
+    {"title": "My Neighbor Totoro", "year": "1998"},
+    {"title": "Dead Poets Society", "year": "1989"},
+    {"title": "Call Me by Your name", "year": "2017"},
+    {"title": "Oppenheimer", "year": "2023"},
+    {"title": "Barbie", "year": "2023"}
+]
+
 
 # 实例化一个对象
 app = Flask(__name__)
@@ -9,9 +20,12 @@ app = Flask(__name__)
 """ Web程序的理解： 编写不同的函数，处理对应url的请求 """
 # 这里的地址是相对地址："/" --> "http://localhost:5000/" ; "/hello" --> "http://localhost:5000/hello"
 @app.route('/hello')
-@app.route('/')
 def hello():
     return '<h1>Welcome to My Watchlist!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route("/")
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 
 @app.route('/user/<name>')
